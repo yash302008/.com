a2e7edf522390347dc8794bcbdc9dfd3a3b6c694
Yash kabadwal <109014733+yash302008@users.noreply.github.com>
2024-10-20 14:00:28 +0530

    added css file

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted! Thank you for contacting us.');
+  });
+  
